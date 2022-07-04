f1fdb2b6952fc1716337ec2c601c9c61105a2356
Brittney <brittneyrichardson8@gmail.com>
2022-07-04 13:31:55 -0500

    updated Nav and gallery

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 import About from './componets/about';
 import Nav from './componets/Nav/index';
+import Gallery from "./componets/Gallery";
 
 function App() {
+  const [categories] = useState([
+    {
+      name: "commercial",
+      desciption:
+        "Photos of grocery stores, food trucks, and other commercials projects",
+    },
+    { name: "Portraits", description: "Portraits of people in my life" },
+    { name: "Food", description: "Delicious delicacies" },
+    {
+      name: "Landscape",
+      desciption: "Fields, Farmhouse, Waterfalls, and the beauty of nature.",
+    },
+  ]);
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
   return (
     <div>
-      <Nav></Nav>
+      <Nav>
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+      </Nav>
       <main>
+        <Gallery></Gallery>
         <About></About>
       </main>
     </div>
