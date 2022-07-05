132539611060fa2f6b14294043517df9eb150f40
Brittney <brittneyrichardson8@gmail.com>
2022-07-05 16:55:27 -0500

    fixed render issue

--- src/App.js
+++ src/App.js
@@ -1,33 +1,30 @@
-import React, {useState} from "react";
-import "./App.css";
-import About from './componets/about';
-import Nav from './componets/Nav/index';
-import Gallery from "./componets/Gallery";
+import React, { useState } from 'react';
+import Nav from './componets/Nav';
+import About from './componets/About';
+import Gallery from './componets/Gallery';
 
 function App() {
   const [categories] = useState([
     {
-      name: "commercial",
-      desciption:
-        "Photos of grocery stores, food trucks, and other commercials projects",
+      name: 'commercial',
+      description: 'Photos of grocery stores, food trucks, and other commercial projects',
     },
-    { name: "Portraits", description: "Portraits of people in my life" },
-    { name: "Food", description: "Delicious delicacies" },
-    {
-      name: "Landscape",
-      desciption: "Fields, Farmhouse, Waterfalls, and the beauty of nature.",
-    },
-  ]);
+    { name: 'portraits', description: 'Portraits of people in my life' },
+    { name: 'food', description: 'Delicious delicacies' },
+    { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
+  ]); 
+
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
   return (
     <div>
-      <Nav>
+      <Nav
         categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
-      </Nav>
+      ></Nav>
       <main>
-        <Gallery></Gallery>
+        <Gallery currentCategory={currentCategory}></Gallery>
         <About></About>
       </main>
     </div>
--- src/componets/Nav/index.js
+++ src/componets/Nav/index.js
@@ -1,38 +1,37 @@
-import React, {useEffect} from "react";
+import React from "react";
 import { capitalizeFirstLetter } from "../../utils/helpers";
 
 function Nav(props) {
-const {
-  categories = [],
-  setCurrentCategory,
-  currentCategory,
-} = props
-  useEffect(() => {
-    document.title = capitalizeFirstLetter(currentCategory.name);
-  }, [currentCategory]);
+  const { categories = [], setCurrentCategory, currentCategory } = props;
+
+  const handleClick = (item) => {
+    console.log(item);
+    return item;
+  };
 
   return (
     <header className="flex-row px-1">
       <h2>
         <a data-testid="link" href="/">
-          <span role="img" aria-label="Camera">
-            {" "}
+          <span role="img" aria-label="camera">
             📸
-          </span>{" "}
+          </span>
           Oh Snap!
         </a>
       </h2>
       <nav>
         <ul className="flex-row">
           <li className="mx-2">
-            <a href="#about">About Me</a>
+            <a data-testid="about" href="#about">
+              About me
+            </a>
           </li>
-          <li> Contact </li>
+          <li className={"mx-2"}>
+            <span onClick={() => handleClick("Contact")}>Contact</span>
+          </li>
           {categories.map((category) => (
             <li
-              className={`mx-1 ${
-                currentCategory.name === category.name && "navActive"
-              }`}
+              className={`mx-1 ${currentCategory.name === category.name}`}
               key={category.name}
             >
               <span
